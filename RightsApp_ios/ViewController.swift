f79aa1bf79e25f509ddc1a83f20d1034646683ea
Jorge <jogoco.idt@gmail.com>
2019-05-28 11:48:45 +0200

    Initial Commit

---
+++ RightsApp_ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RightsApp_ios
+//
+//  Created by Jorge Gonzalez Conejero on 28/05/2019.
+//  Copyright © 2019 uab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
