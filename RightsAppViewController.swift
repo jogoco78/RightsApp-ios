9e735cce16f7c74e4de01596aa6da0f0bb802f0a
Jorge <jogoco.idt@gmail.com>
2019-06-04 17:00:48 +0200

    New VC

---
+++ RightsAppViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RightsAppViewController.swift
+//  RightsApp_ios
+//
+//  Created by Jorge Gonzalez Conejero on 04/06/2019.
+//  Copyright © 2019 uab. All rights reserved.
+//
+
+import UIKit
+
+class RightsAppViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- TermsConditionsViewController.swift
+++ TermsConditionsViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class TermsConditionsViewController: UIViewController {
     
     @IBOutlet weak var tv_termsConditions: UITextView!
+    @IBOutlet weak var acceptTermsSwitch: UISwitch!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -69,18 +70,56 @@ class TermsConditionsViewController: UIViewController {
         s = NSMutableAttributedString(string: NSLocalizedString("privacyPolicyChangesBody",comment: "Comment"), attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 12), NSAttributedString.Key.paragraphStyle: paragraphStyle])
         attributedText.append(s)
         
-        //Sets the attributed texts to the TextView
+        //Sets the attributed text to the TextView
         tv_termsConditions.attributedText = attributedText
         
-        
-        //s = NSLocalizedString("uIy-gh-e7p.text", comment: "")
-        //tv_ownership_body.text = s
+        //Sets the switch to the stored value
+        if UserDefaults.standard.object(forKey: "agreed") != nil {
+            acceptTermsSwitch.setOn(UserDefaults.standard.bool(forKey: "agreed"), animated: true)
+        } else {
+            acceptTermsSwitch.setOn(false, animated: true)
+        }
+    }
     
-        //print(s)
+    //Stores if the user agreeds with the terms and conditions
+    @IBAction func switchChangeTerms(_ sender: UISwitch) {
+        
+    }
+    
+    @IBAction func buttonAcceptTerms(_ sender: UIButton) {
+        if acceptTermsSwitch.isOn {
+            //Stores preferences and launch next VC
+            UserDefaults.standard.set(true, forKey: "agreed")
+            
+            if UserDefaults.standard.object(forKey: "useApp") != nil {
+                if UserDefaults.standard.bool(forKey: "useApp") {
+                    //it is true that the user DO NOT want to see the explanation again
+                    performSegue(withIdentifier: "RightsAppSegue", sender: nil)
+                }else{
+                    performSegue(withIdentifier: "HowToUseTheAppSegue", sender: nil)
+                }
+            } else {
+                UserDefaults.standard.set(false, forKey: "useApp")
+                performSegue(withIdentifier: "HowToUseTheAppSegue", sender: nil)
+            }
+            
+        } else {
+            //Shows an alert: temrs and conditions must be accepted
+            let alert = UIAlertController(title: nil, message: NSLocalizedString("alertTermsConditions", comment: ""), preferredStyle: .alert)
+            alert.view.backgroundColor = UIColor.black
+            alert.view.alpha = 0.6
+            alert.view.layer.cornerRadius = 15
+            
+            self.present(alert, animated: true)
+            
+            DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 3){
+                alert.dismiss(animated: true)
+            }
+        }
     }
     
     
-   
+    
     /*
     // MARK: - Navigation
 
