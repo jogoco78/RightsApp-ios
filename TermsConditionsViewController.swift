e97dfcc4d5a8376b02d0eef170f9c45b8686f920
Jorge <jogoco.idt@gmail.com>
2019-06-04 12:34:00 +0200

    Terms and Conditions in English

--- TermsConditionsViewController.swift
+++ TermsConditionsViewController.swift
@@ -9,21 +9,78 @@
 import UIKit
 
 class TermsConditionsViewController: UIViewController {
-
-    @IBOutlet weak var tv_ownership: UITextView!
+    
+    @IBOutlet weak var tv_termsConditions: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        let s = NSLocalizedString("xuQ-ZP-tMN.text",comment: "Comment")
-        tv_ownership.text = s
         
-        print(s)
+        //line spacing
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineSpacing = 3
+        
+        //App ownership
+        var s = NSMutableAttributedString(string: NSLocalizedString("appOwnership",comment: "Comment"), attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 24), NSAttributedString.Key.paragraphStyle: paragraphStyle])
+        let attributedText = s
+        s = NSMutableAttributedString(string: NSLocalizedString("appOwnershipBody",comment: "Comment"), attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 12), NSAttributedString.Key.paragraphStyle: paragraphStyle])
+        attributedText.append(s)
+        
+        //Terms and conditions
+        s = NSMutableAttributedString(string: NSLocalizedString("termsConditions",comment: "Comment"), attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 24), NSAttributedString.Key.paragraphStyle: paragraphStyle])
+        attributedText.append(s)
+        s = NSMutableAttributedString(string: NSLocalizedString("termsConditionsBody",comment: "Comment"), attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 12), NSAttributedString.Key.paragraphStyle: paragraphStyle])
+        attributedText.append(s)
+        
+        //Privacy policy
+        s = NSMutableAttributedString(string: NSLocalizedString("privacyPolicy",comment: "Comment"), attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 24), NSAttributedString.Key.paragraphStyle: paragraphStyle])
+        attributedText.append(s)
+        s = NSMutableAttributedString(string: NSLocalizedString("privacyPolicyBody",comment: "Comment"), attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 12), NSAttributedString.Key.paragraphStyle: paragraphStyle])
+        attributedText.append(s)
+        
+        //Personal data
+        s = NSMutableAttributedString(string: NSLocalizedString("personalData",comment: "Comment"), attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 18), NSAttributedString.Key.paragraphStyle: paragraphStyle])
+        attributedText.append(s)
+        s = NSMutableAttributedString(string: NSLocalizedString("personalDataBody",comment: "Comment"), attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 12), NSAttributedString.Key.paragraphStyle: paragraphStyle])
+        attributedText.append(s)
+        
+        //Links to external sites
+        s = NSMutableAttributedString(string: NSLocalizedString("linksToExternalSites",comment: "Comment"), attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 18), NSAttributedString.Key.paragraphStyle: paragraphStyle])
+        attributedText.append(s)
+        s = NSMutableAttributedString(string: NSLocalizedString("linksToExternalSitesBody",comment: "Comment"), attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 12), NSAttributedString.Key.paragraphStyle: paragraphStyle])
+        attributedText.append(s)
+        
+        //Aggregated statistics
+        s = NSMutableAttributedString(string: NSLocalizedString("aggregatedStatistics",comment: "Comment"), attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 18), NSAttributedString.Key.paragraphStyle: paragraphStyle])
+        attributedText.append(s)
+        s = NSMutableAttributedString(string: NSLocalizedString("aggregatedStatisticsBody",comment: "Comment"), attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 12), NSAttributedString.Key.paragraphStyle: paragraphStyle])
+        attributedText.append(s)
+        
+        //Google Analytics
+        s = NSMutableAttributedString(string: NSLocalizedString("googleAnalytics",comment: "Comment"), attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 18), NSAttributedString.Key.paragraphStyle: paragraphStyle])
+        attributedText.append(s)
+        s = NSMutableAttributedString(string: NSLocalizedString("googleAnalyticsBody",comment: "Comment"), attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 12), NSAttributedString.Key.paragraphStyle: paragraphStyle])
+        attributedText.append(s)
+        
+        //Privacy policy changes
+        s = NSMutableAttributedString(string: NSLocalizedString("privacyPolicyChanges",comment: "Comment"), attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 18), NSAttributedString.Key.paragraphStyle: paragraphStyle])
+        attributedText.append(s)
+        s = NSMutableAttributedString(string: NSLocalizedString("privacyPolicyChangesBody",comment: "Comment"), attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 12), NSAttributedString.Key.paragraphStyle: paragraphStyle])
+        attributedText.append(s)
+        
+        //Sets the attributed texts to the TextView
+        tv_termsConditions.attributedText = attributedText
+        
+        
+        //s = NSLocalizedString("uIy-gh-e7p.text", comment: "")
+        //tv_ownership_body.text = s
+    
+        //print(s)
     }
     
     
-    
+   
     /*
     // MARK: - Navigation
 
