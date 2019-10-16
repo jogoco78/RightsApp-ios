542e0c4460aba3afa4c000776d43902e3d8730b4
Jorge <jogoco.idt@gmail.com>
2019-10-16 19:30:23 +0200

    GoogleAnalytics Firebase libraties

---
+++ Pods/Target Support Files/Pods-RightsApp/Pods-RightsApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RightsApp : NSObject
+@end
+@implementation PodsDummy_Pods_RightsApp
+@end
---
+++ Pods/Target Support Files/Pods-RightsAppTests/Pods-RightsAppTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RightsAppTests : NSObject
+@end
+@implementation PodsDummy_Pods_RightsAppTests
+@end
